05995605728b4831020b96d65e1d3d2326ca05d4
mmrydz <megrydzewski@gmail.com>
2019-05-14 15:14:20 -0400

    updated for seeding data

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const db = require("../models");
+
+const db = require("../server/database/models");
 
 // This file empties the Books collection and inserts the books below
 
@@ -12,12 +13,10 @@ const userSeed = [
   {
     username: "admin1",
     password: "password1",
-    date: new Date(Date.now())
   },
   {
     username: "admin2",
     password: "password2",
-    date: new Date(Date.now())
   }
 ];
 
