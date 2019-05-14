ca570ee5e9f68d451b175f473b6ebe87a0b5b299
mmrydz <megrydzewski@gmail.com>
2019-05-14 14:24:26 -0400

    added seedDB.js

---
+++ server/database/seedDB.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/tag-this-app"
+);
+
+const userSeed = [
+  {
+    username: "admin1",
+    password: "password1",
+    date: new Date(Date.now())
+  },
+  {
+    username: "admin2",
+    password: "password2",
+    date: new Date(Date.now())
+  }
+];
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
