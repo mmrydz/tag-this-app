c0713ddca0a072c95feafb4af718d76da67e5f79
arthurdoelp <arthurdoelp@gmail.com>
2019-05-16 12:04:19 -0400

    copied over the login/signup src files

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,103 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import itemManage from "./pages/itemManage";
+import React, { Component } from 'react';
+import axios from 'axios'
+import { Route, Link } from 'react-router-dom'
+// components
+import Signup from './components/sign-up'
+import LoginForm from './components/login-form'
+import Navbar from './components/navbar'
+import Home from './components/home'
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={itemManage} />
-        </Switch>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+   
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        {/* greet user if logged in: */}
+        {this.state.loggedIn &&
+          <p>Join the party, {this.state.username}!</p>
+        }
+        {/* Routes to different components */}
+        <Route
+          exact path="/"
+          component={Home} />
+        <Route
+          path="/login"
+          render={() =>
+            <LoginForm
+              updateUser={this.updateUser}
+            />}
+        />
+        <Route
+          path="/signup"
+          render={() =>
+            <Signup/>}
+        />
+
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import itemManage from "./pages/itemManage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Switch>
+//           <Route exact path="/" component={itemManage} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
