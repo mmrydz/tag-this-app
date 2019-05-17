a671180b6bdc2f72ee8fc6e569d4183dc099be6b
Ben Leone <benleone90@users.noreply.github.com>
2019-05-17 14:35:03 -0400

    added useNewURLParser to mongoosedb

--- server/database/index.js
+++ server/database/index.js
@@ -1,24 +1,23 @@
 //Connect to Mongo database
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/tag-this-app';
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/tag-this-app";
 
-mongoose.connect(uri).then(
-    () => { 
-        /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
-        console.log('Connected to Mongo');
-        
-    },
-    err => {
-         /** handle initial connection error */ 
-         console.log('error connecting to Mongo: ')
-         console.log(err);
-         
-        }
-  );
+mongoose.connect(uri, { useNewUrlParser: true }).then(
+  () => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
 
+    console.log("Connected to Mongo");
+  },
+  err => {
+    /** handle initial connection error */
 
-module.exports = mongoose.connection
+    console.log("error connecting to Mongo: ");
+    console.log(err);
+  }
+);
+
+module.exports = mongoose.connection;
