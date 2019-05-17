0b91169c398e4b091e2abb4adfe32816b36a177f
mmrydz <megrydzewski@gmail.com>
2019-05-17 12:39:32 -0400

    fresh start

--- client/src/index.js
+++ client/src/index.js
@@ -14,22 +14,4 @@ ReactDOM.render(
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
-);
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App.js";
-// import { BrowserRouter } from "react-router-dom"; //don't need to specify localhost url in axios http address
-
-// //style
-// // import "spectre.css/dist/spectre.min.css";
-// // import "spectre.css/dist/spectre-icons.css";
-// // import "./index.css";
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
+)
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://db_user1:db_password1@ds147942.mlab.com:47942/heroku_7290ngn4",
-  secretOrKey: "secret"
-};
