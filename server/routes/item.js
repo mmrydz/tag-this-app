3c1007459afb15037aa0a9d36c787cc628fdf528
mmrydz <megrydzewski@gmail.com>
2019-05-17 15:57:29 -0400

    routes and controllers/models for items

---
+++ server/routes/item.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const router = require("express").Router();
+const itemController = require("../database/controllers/itemController");
+
+// Matches with "/items"
+router.route("/")
+  .get(itemController.findAll)
+  .post(itemController.create);
+
+// Matches with "/items/:id"
+router
+  .route("/:id")
+  .get(itemController.findById)
+  .put(itemController.update)
+  .delete(itemController.remove);
+
+module.exports = router;
