a80bd1dd6f221d9b229282f3cc6b0b430ed4ba46
Ben Leone <benleone90@users.noreply.github.com>
2019-05-20 15:07:12 -0400

    updated text input

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,7 @@
 // //import { withAuthenticator } from "aws-amplify-react";
 // //import { Storage } from "aws-amplify";
 
-
 // function App() {
-
-
 
 //   return (
 //     <Router>
@@ -37,10 +34,8 @@
 
 // export default App;
 
-
-
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 //import { Route, Link } from 'react-router-dom'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // pages
@@ -48,15 +43,15 @@ import ItemManagePage from "./pages/itemManage";
 import AdminHomePage from "./pages/admin-home";
 import UpdateItemPage from "./pages/updateform";
 // components
-import Signup from './components/sign-up'
-import LoginForm from './components/login-form'
-import Navbar from './components/Navbar/index'
+import Signup from "./components/sign-up";
+import LoginForm from "./components/login-form";
+import Navbar from "./components/Navbar/index";
 //import Home from './components/home'
-import Items from './components/Items'
+import Items from "./components/Items";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       loggedIn: false,
       username: null,
@@ -64,7 +59,8 @@ class App extends Component {
         {
           id: 1,
           name: "tent",
-          description: "Put a lot of text and words in this space to describe the item, blah blah blah",
+          description:
+            "Put a lot of text and words in this space to describe the item, blah blah blah",
           image: "https://www.rei.com/media/product/130893",
           department: "outdoor",
           favorite: false,
@@ -73,8 +69,10 @@ class App extends Component {
         {
           id: 2,
           name: "bike",
-          description: "Put a lot of text and words in this space to describe the item, blah blah blah",
-          image: "https://images-na.ssl-images-amazon.com/images/I/915c7fzsEBL._SX425_.jpg",
+          description:
+            "Put a lot of text and words in this space to describe the item, blah blah blah",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/915c7fzsEBL._SX425_.jpg",
           department: "outdoor",
           favorite: false,
           featured: true
@@ -82,8 +80,10 @@ class App extends Component {
         {
           id: 3,
           name: "frank and beans",
-          description: "Put a lot of text and words in this space to describe the item, blah blah blah",
-          image: "https://images-na.ssl-images-amazon.com/images/I/51tTHFbUkRL.jpg",
+          description:
+            "Put a lot of text and words in this space to describe the item, blah blah blah",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/51tTHFbUkRL.jpg",
           department: "grocery",
           favorite: false,
           featured: false
@@ -91,114 +91,101 @@ class App extends Component {
         {
           id: 4,
           name: "pants",
-          description: "Put a lot of text and words in this space to describe the item, blah blah blah",
-          image: "https://images-na.ssl-images-amazon.com/images/I/71uCuqRv43L._UY445_.jpg",
+          description:
+            "Put a lot of text and words in this space to describe the item, blah blah blah",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/71uCuqRv43L._UY445_.jpg",
           department: "clothing",
           favorite: false,
           featured: false
         }
       ]
-    }
+    };
 
-    this.getUser = this.getUser.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.updateUser = this.updateUser.bind(this)
-    this.saveFavorite = this.saveFavorite.bind(this)
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+    this.saveFavorite = this.saveFavorite.bind(this);
   }
 
   componentDidMount() {
-    this.getUser()
+    this.getUser();
   }
 
   updateUser(userObject) {
-    this.setState(userObject)
+    this.setState(userObject);
   }
 
   getUser() {
-    axios.get('/user/').then(response => {
-      console.log('Get user response: ')
-      console.log(response.data)
+    axios.get("/user/").then(response => {
+      console.log("Get user response: ");
+      console.log(response.data);
       if (response.data.user) {
-        console.log('Get User: There is a user saved in the server session: ')
+        console.log("Get User: There is a user saved in the server session: ");
 
         this.setState({
           loggedIn: true,
           username: response.data.user.username
-        })
+        });
       } else {
-        console.log('Get user: no user');
+        console.log("Get user: no user");
         this.setState({
           loggedIn: false,
           username: null
-        })
+        });
       }
-    })
+    });
   }
 
-  saveFavorite = (id) => {
-    console.log(id + 'favorite saved! TEST from app.js')
+  saveFavorite = id => {
+    console.log(id + "favorite saved! TEST from app.js");
     this.setState({
       items: this.state.items.map(item => {
         if (item.id === id) {
-          item.favorite = !item.favorite
+          item.favorite = !item.favorite;
         }
         return item;
       })
     });
-  }
+  };
 
   render() {
     return (
       <Router>
         <div className="App">
-          <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+          {/* <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} /> */}
           {/* greet user if logged in: */}
-          {this.state.loggedIn &&
-            <p>Join the party, {this.state.username}!</p>
-          }
+          {this.state.loggedIn && <p>Join the party, {this.state.username}!</p>}
           {/* Routes to different components */}
           <Switch>
+            <Route exact path="/adminhome" render={() => <AdminHomePage />} />
+            <Route exact path="/manage" render={() => <ItemManagePage />} />
+            <Route exact path="/update" render={() => <UpdateItemPage />} />
             <Route
-              exact path="/adminhome"
-              render={() => <AdminHomePage />}
-            />
-            <Route
-              exact path="/manage"
-              render={() => <ItemManagePage />}
-            />
-            <Route
-              exact path="/update"
-              render={() => <UpdateItemPage />}
-            />
-            <Route
-              exact path="/"
-              render={() =>
+              exact
+              path="/"
+              render={() => (
                 <Items
                   items={this.state.items}
                   saveFavorite={this.saveFavorite}
                   isFeatured={this.isFeatured}
-                />}
+                />
+              )}
             />
             <Route
               path="/login"
-              render={() =>
-                <LoginForm
-                  updateUser={this.updateUser}
-                />}
+              render={() => <LoginForm updateUser={this.updateUser} />}
             />
-            <Route
-              path="/signup"
-              render={() =>
-                <Signup />}
-            />
+            <Route path="/signup" render={() => <Signup />} />
             <Route
               path="/items"
-              render={() =>
+              render={() => (
                 <Items
                   items={this.state.items}
                   saveFavorite={this.saveFavorite}
                   isFeatured={this.isFeatured}
-                />}
+                />
+              )}
             />
           </Switch>
         </div>
--- client/src/pages/itemManage.js
+++ client/src/pages/itemManage.js
@@ -1,11 +1,54 @@
 import React, { Component } from "react";
-import { Input } from "../components/ManageItem/newindex";
+import {
+  Input,
+  CategoryInput,
+  QualityInput
+} from "../components/ManageItem/newindex";
 
 class ManageItemPage extends Component {
+  state = {
+    barcode: "",
+    name: "",
+    price: "10.00"
+  };
+
   render() {
     return (
-      <div className="container d-flex justify-content-center">
-        THIS IS TEST TEXT
+      <div className="container">
+        <h3>THIS IS TEST TEXT!</h3>
+        <div className="justify-content-center">
+          <form>
+            <Input
+              type="text"
+              // value={this.state.barcode}
+              // onChange={this.handleInputChange}
+              name="barcode"
+              placeholder="Item Barcode"
+            />
+            <Input
+              type="text"
+              // value={this.state.name}
+              // onChange={this.handleInputChange}
+              name="itemName"
+              placeholder="Item Name"
+            />
+            <div class="input-group">
+              <span class="input-group-addon">$</span>
+              <input
+                type="number"
+                placeholder="Enter price"
+                min="0"
+                step="0.01"
+                data-number-to-fixed="2"
+                data-number-stepfactor="100"
+                class="form-control currency"
+                id="price"
+              />
+            </div>
+            <CategoryInput id="category-input" />
+            <QualityInput id="quality-input" />
+          </form>
+        </div>
       </div>
     );
   }
