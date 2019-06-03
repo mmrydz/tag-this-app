9e8ad1f08699dbbf4227211098eae5f97a8c7a79
mmrydz <megrydzewski@gmail.com>
2019-06-03 12:18:01 -0400

    hitting rewind

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        adminhome: './adminhome',
+        itemmanage: './itemmanage',
+        items: './items',
+        update: './update'
+      }
+    };
